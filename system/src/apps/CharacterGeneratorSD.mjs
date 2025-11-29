a37f47254bb76939b2340fd4a833b81134cc4db3
Britton Johnson <brittonlj@gmail.com>
2025-11-29 09:34:56 -0600

    BUG 1172: Added names to random name tables injects HTML

--- system/src/apps/CharacterGeneratorSD.mjs
+++ system/src/apps/CharacterGeneratorSD.mjs
@@ -908,7 +908,10 @@ export default class CharacterGeneratorSD extends foundry.appv1.api.FormApplicat
 		const table = await fromUuid(this.ancestry.system.nameTable);
 		if (table) {
 			const result = await table.draw({displayChat: false});
-			this.formData.actor.name = result.results[0].text;
+			// Remove HTML from description if no name
+			this.formData.actor.name = (result.results[0].name)
+				? result.results[0].name
+				: result.results[0].text.replace(/<[^>]+>/g, "");
 		}
 		else {
 			this.formData.actor.name = `Unnamed ${this.ancestry.name}`;
