3b21555481305c15c0a657dbc275708535b05eb6
Britton Johnson <brittonlj@gmail.com>
2025-11-29 10:24:57 -0600

    Missed updates

--- system/src/apps/LightSourceTrackerSD.mjs
+++ system/src/apps/LightSourceTrackerSD.mjs
@@ -184,6 +184,7 @@ export default class LightSourceTrackerSD extends foundry.appv1.api.Application 
 				speaker: speaker ?? ChatMessage.getSpeaker(),
 				picked_up: false,
 				template: "systems/shadowdark/templates/chat/lightsource-drop.hbs",
+				showRemainingMins: game.settings.get("shadowdark", "playerShowLightRemaining") > 1,
 			}
 		);
 
@@ -261,6 +262,7 @@ export default class LightSourceTrackerSD extends foundry.appv1.api.Application 
 			actor,
 			item,
 			picked_up: true,
+			showRemainingMins: game.settings.get("shadowdark", "playerShowLightRemaining") > 1,
 		};
 
 		let template = "systems/shadowdark/templates/chat/lightsource-drop.hbs";
